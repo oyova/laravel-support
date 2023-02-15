939f217cba2412b281f4a5d844727cb8d2e270c4
Ryan Whitman <ryanawhitman@gmail.com>
2023-02-15 11:55:31 -0500

    Add helpers and macros

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,71 @@
 <?php
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+if (! function_exists('isLocal')) {
+    function isLocal(): bool
+    {
+        return app()->environment('local');
+    }
+}
+
+if (! function_exists('isTesting')) {
+    function isTesting(): bool
+    {
+        return app()->environment('testing');
+    }
+}
+
+if (! function_exists('isStaging')) {
+    function isStaging(): bool
+    {
+        return app()->environment('staging');
+    }
+}
+
+if (! function_exists('isProduction')) {
+    function isProduction(): bool
+    {
+        return app()->environment('production');
+    }
+}
+
+if (! function_exists('authUser')) {
+    function authUser(): ?User
+    {
+        return auth()->user();
+    }
+}
+
+if (! function_exists('authUserId')) {
+    function authUserId(): ?int
+    {
+        return auth()->check() ? authUser()->id : null;
+    }
+}
+
+if (! function_exists('str')) {
+    /**
+     * This function exists in Laravel >= 9.
+     */
+    function str($string = null)
+    {
+        if (func_num_args() === 0) {
+            return new class()
+            {
+                public function __call($method, $parameters)
+                {
+                    return Str::$method(...$parameters);
+                }
+
+                public function __toString()
+                {
+                    return '';
+                }
+            };
+        }
+
+        return Str::of($string);
+    }
+}
---
+++ src/macros.php
@@ -0,0 +1,79 @@
+<?php
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
+use Laravel\Nova\Fields\Field;
+
+if (
+    class_exists(Stringable::class) &&
+    ! method_exists(Stringable::class, 'squish')
+) {
+    /**
+     * This method exists in Laravel >= 9.
+     */
+    Stringable::macro('squish', function (): Stringable {
+        return new static(Str::squish($this->value));
+    });
+}
+
+if (
+    class_exists(Str::class) &&
+    ! method_exists(Str::class, 'squish')
+) {
+    /**
+     * This method exists in Laravel >= 9.
+     */
+    Str::macro('squish', function ($value): string {
+        return preg_replace(
+            '~(\s|\x{3164})+~u',
+            ' ',
+            preg_replace('~^[\s﻿]+|[\s﻿]+$~u', '', $value)
+        );
+    });
+}
+
+if (
+    class_exists(Collection::class) &&
+    ! method_exists(Collection::class, 'rescueEach')
+) {
+    Collection::macro(
+        'rescueEach',
+        function (callable $callback, $rescue = null, $report = true) {
+            return $this->each(
+                function ($item, $key) use ($callback, $rescue, $report) {
+                    return rescue(
+                        function () use ($callback, $item, $key) {
+                            return $callback($item, $key);
+                        },
+                        $rescue,
+                        $report
+                    );
+                }
+            );
+        }
+    );
+}
+
+if (
+    class_exists(Field::class) &&
+    ! method_exists(Field::class, 'showOnPeekAndPreview')
+) {
+    Field::macro('showOnPeekAndPreview', function (): Field {
+        return $this
+            ->showWhenPeeking()
+            ->showOnPreview();
+    });
+}
+
+if (
+    class_exists(Field::class) &&
+    ! method_exists(Field::class, 'showOnAllExceptForms')
+) {
+    Field::macro('showOnAllExceptForms', function (): Field {
+        return $this
+            ->showOnIndex()
+            ->showOnPeekAndPreview()
+            ->showOnDetail();
+    });
+}
